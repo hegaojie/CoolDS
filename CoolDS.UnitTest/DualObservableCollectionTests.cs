f7a4337c68900f3cc429d8630ee988491fcb6d0a
Gaojie He <hgj320@hotmail.com>
2019-09-13 15:30:03 +0200

    '.'

---
+++ CoolDS.UnitTest/DualObservableCollectionTests.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace CoolDS.UnitTest
+{
+    [TestFixture]
+    public class DualObservableCollectionTests
+    {
+        [Test]
+        public void DefaultConstructorShouldSetVisualSizeTo0()
+        {
+            var collection = new DualObserableCollection<int>();
+            Assert.AreEqual(collection.VisualSize, 0);
+        }
+
+        [Test]
+        public void SetVisualSizeInConstructor()
+        {
+            var collection = new DualObserableCollection<int>(5);
+            Assert.AreEqual(collection.VisualSize, 5);
+        }
+
+        [TestCase(1, Description = "VisualSize less than collection size")]
+        [TestCase(5, Description = "VisualSize greater than collection size")]
+        public void ConstructorWithFullParameters(int visualSize)
+        {
+            var source = new List<int>(){1, 2, 3};
+            var collection = new DualObserableCollection<int>(source, visualSize);
+            Assert.AreEqual(collection.VisualSize, visualSize);
+        }
+
+        [Test]
+        public void CanNotMove1StepToLeftIfCollectionEmpty()
+        {
+            var collection = new DualObserableCollection<int>();
+            Assert.IsFalse(collection.CanMove1StepToLeft);
+        }
+
+        [Test]
+        public void CanMove1StepToLeftByConstructorWithCollectionAndSmallerVisualSize()
+        {
+            var collection = new DualObserableCollection<int>(new List<int>{1, 2, 3, 4, 5}, 2);
+            Assert.IsTrue(collection.CanMove1StepToLeft);
+        }
+
+        [TestCase(3, Description = "Steps less equal to maximum steps to Left")]
+        [TestCase(5, Description = "More than maximum steps to Left")]
+        public void MoveStepsToLeft(int steps)
+        {
+            var collection = new DualObserableCollection<int>(new List<int> { 1, 2, 3, 4, 5 }, 2);
+            collection.MoveStepsToLeft(steps);
+            var firstVisible = collection.VisualElements.First();
+            var lastVisibile = collection.VisualElements.Last();
+            Assert.AreEqual(4, firstVisible, "First visual element should be 4");
+            Assert.AreEqual(5, lastVisibile, "Last visual element should be 5");
+        }
+
+        [TestCase(null, 0, Description = "Empty collection")]
+        [TestCase(new int[] { 1, 2, 3 }, 2, Description = "VisualSize is less than count")]
+        [TestCase(new int[] { 1, 2, 3 }, 5, Description = "VisualSize is greater than count")]
+        public void CanNotMove1StepToRightJustAfterConstruction(ICollection<int> source, int visualSize)
+        {
+            var collection = new DualObserableCollection<int>(source, visualSize);
+            Assert.IsFalse(collection.CanMove1StepToRight);
+        }
+
+        [TestCase(3, Description = "Steps less equal to maximum steps to right")]
+        [TestCase(5, Description = "More than maximum steps to right")]
+        public void MoveStepsToRight(int steps)
+        {
+            var collection = new DualObserableCollection<int>(new List<int> { 1, 2, 3, 4, 5 }, 2);
+            collection.MoveStepsToLeft(5);
+
+            collection.MoveStepsToRight(steps);
+
+            var firstVisible = collection.VisualElements.First();
+            var lastVisibile = collection.VisualElements.Last();
+            Assert.AreEqual(1, firstVisible, "First visual element should be 1");
+            Assert.AreEqual(2, lastVisibile, "Last visual element should be 2");
+        }
+    }
+}
---
+++ CoolDS/DualObserableCollection.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace CoolDS
+{
+    public class DualObserableCollection<T> where T : IComparable, new ()
+    {
+
+        public DualObserableCollection() : this(null, 0)
+        { }
+
+        public DualObserableCollection(int visualSize) : this(null, visualSize)
+        { }
+
+        public DualObserableCollection(IEnumerable<T> source) : this(source, source?.Count() ?? 0)
+        { }
+
+        public DualObserableCollection(IEnumerable<T> source, int visualSize)
+        {
+            VisualSize = visualSize;
+            VisualElements = new ObservableCollection<T>();
+            _elements = new List<T>();
+            if (source == null) return;
+            _elements.AddRange(source);
+            InitializeVisualElements(_elements.Take(visualSize));
+        }
+
+        private void InitializeVisualElements(IEnumerable<T> source)
+        {
+            VisualElements.Clear();
+            foreach (var element in source)
+                VisualElements.Add(element);
+        }
+
+        public ObservableCollection<T> VisualElements { get; }
+
+        public int VisualSize { get; set; }
+
+        public int Count => _elements.Count;
+
+        public bool CanMove1StepToLeft => CalculateActualStepsToLeft(1) > 0;
+
+        public bool CanMove1StepToRight => CalculateActualStepsToRight(1) > 0;
+
+        /// <summary>
+        /// Return the minimum value of expected steps and maximum of possible steps to left
+        /// </summary>
+        /// <param name="steps">Expected steps to be moved to left</param>
+        /// <returns>Actual steps can be moved to left</returns>
+        private int CalculateActualStepsToLeft(int steps)
+        {
+            var originalVisualEnd = _visualStart + VisualSize;
+            return originalVisualEnd + steps > Count ? Count - originalVisualEnd : steps;
+        }
+        
+        /// <summary>
+        /// Return the minimum value of expected steps and maximum of possible steps to right
+        /// </summary>
+        /// <param name="steps">Expected steps to be moved to right</param>
+        /// <returns>Actual steps can be moved to right</returns>
+        private int CalculateActualStepsToRight(int steps)
+        {
+            var newVisualStart = _visualStart - steps;
+            return newVisualStart < 0 ? steps + newVisualStart : steps;
+        }
+
+        public void MoveStepsToLeft(int steps)
+        {
+            MoveSteps(steps, 1);
+        }
+
+        public void MoveStepsToRight(int steps)
+        {
+            MoveSteps(steps, -1);
+        }
+
+        private void MoveSteps(int steps, int direction)
+        {
+            var actualSteps = direction < 0 ? CalculateActualStepsToRight(steps) : CalculateActualStepsToLeft(steps);
+            if (actualSteps < 1) return;
+            _visualStart += actualSteps * direction;
+            InitializeVisualElements(TakeElements(_visualStart, _visualStart + actualSteps - 1));
+        }
+
+        private IEnumerable<T> TakeElements(int start, int end)
+        {
+            for (var i = start; i < end; i++)
+                yield return _elements[i];
+        }
+
+        public void Clear()
+        {
+            _visualStart = 0;
+            _elements.Clear();
+            VisualElements.Clear();
+        }
+
+        private int _visualStart;
+        private List<T> _elements;
+    }
+}
